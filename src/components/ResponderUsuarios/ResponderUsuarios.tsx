2d50696eb7c9f93c6b5ae7ffca960a76cdbad308
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2024-04-24 16:12:47 -0400

    cambios de microfono

---
+++ src/components/ResponderUsuarios/ResponderUsuarios.tsx
@@ -0,0 +1,11 @@
+import "./ResponderUsuarios.styles.css"
+
+const ResponderUsuarios = () => {
+  return (
+    <main className=' window-content '>
+        <iframe className="chat-box" src="http://localhost:4000/salas"></iframe>
+    </main>
+  )
+}
+
+export default ResponderUsuarios
